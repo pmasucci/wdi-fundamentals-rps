45deec7293e16b8827f9ce55a0322e565782c817
Peter Masucci <peter.a.masucci@gmail.com>
2015-04-04 15:31:01 -0400

    define playToFive().

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -75,7 +75,19 @@ function playToFive() {
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    while (playerWins < 5 && computerWins < 5){
+        playerMove = getPlayerMove();
+        computerMove = getComputerMove();
+        winner = getWinner(playerMove, computerMove);
+        if (winner === 'player'){
+            playerWins += 1;
+        }
+        else if(winner === 'computer'){
+            computerWins += 1;
+        }
+        console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+        console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+    }
     return [playerWins, computerWins];
 }
 
